ea2a413809abae7fa903202271848eb895b59346
Christian Peter <christian.peter@knowit.no>
2017-02-09 16:00:48 +0100

    Adds beer products

--- src/Products/Products.js
+++ src/Products/Products.js
@@ -10,6 +10,9 @@ export default class Products extends Component {
   **/
 
   render() {
+    const beerProducts = [
+      {"id":1,"name":"Nøgne Ø Imperial Stout","price":80,"img":"https://bilder.vinmonopolet.no/cache/300x300-0/1053802-1.jpg"},
+    ]
     return (
       <div className="Products">
         // Do something crazy with beer in here.
