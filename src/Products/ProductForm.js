fe1f2bed3bf5994349512dd7f5d1918562e85bcd
Øyvind Marthinsen <oyvinmar@gmail.com>
2019-09-09 18:22:35 +0000

    Use hooks

--- src/Products/ProductForm.js
+++ src/Products/ProductForm.js
@@ -1,43 +1,38 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Input from '../Input/Input';
 
 const defaultState = {
   name: '',
   price: 0,
   img: '',
-}
+};
 
-class ProductForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = defaultState;
-  }
+function ProductForm(props) {
+  let [product, setProduct] = useState(defaultState);
 
-  onFieldChange = (event) => {
+  let onFieldChange = event => {
     const value = event.target.value;
     const name = event.target.name;
     // Remove this when you get it working
-    console.log(`field ${name} changed to value: ${value}`)
+    console.log(`field ${name} changed to value: ${value}`);
     // Update the state based on the input field changed
-  }
+  };
 
-  submitProduct = (evt) => {
+  let submitProduct = evt => {
     // We don't want to reload on submit
     evt.preventDefault();
     // Make sure that the form is reset by setting the state back to the default
-  }
+  };
 
-  render() {
-    const { name, price, img } = this.state;
-    return (
-      <form>
-        <Input name="name" type="text" label="Name: " value={name} />
-        <Input name="price" type="number" label="Price: " value={price} />
-        <Input name="img" type="text" label="Image: " value={img} />
-        <Input name="submit" type="submit"/>
-      </form>
-    )
-  }
+  const { name, price, img } = product;
+  return (
+    <form>
+      <Input name="name" type="text" label="Name: " value={name} />
+      <Input name="price" type="number" label="Price: " value={price} />
+      <Input name="img" type="text" label="Image: " value={img} />
+      <Input name="submit" type="submit" />
+    </form>
+  );
 }
 
 export default ProductForm;
