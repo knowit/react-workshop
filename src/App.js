7bb314b148627472387cf192318e2b5a188975c4
Oyvind Marthinsen <oyvinmar@gmail.com>
2019-09-18 12:49:19 +0000

    convert App to function compomnent

--- src/App.js
+++ src/App.js
@@ -1,27 +1,22 @@
-import React, { Component } from 'react';
+import React  from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Products from './Products/Products'
 import Header from './Header';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <Header title="Maybe you should do something here?"/>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Products />
+function App()  {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <Header title="Maybe you should do something here?"/>
       </div>
-    );
-  }
+      <p className="App-intro">
+        To get started, edit <code>src/App.js</code> and save to reload.
+      </p>
+      <Products />
+    </div>
+  );
 }
 
 export default App;
